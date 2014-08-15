83228b0d4cd687ea53e38c10669cc4cdc7f23770
55011212028Supakiat <imacatMSU>
2014-08-15 16:28:40 +0700

    เขียนใหม่หมดเลย

--- swifttutorial_1.playground/section-1.swift
+++ swifttutorial_1.playground/section-1.swift
@@ -1,10 +1,24 @@
 let tutorialTeam = 56
 let editorialTeam = 23
-let totalTeam = tutorialTeam + editorialTeam
+var totalTeam = tutorialTeam + editorialTeam
 
 totalTeam += 1
-let priceInferred = -19
-let priceInExplict:Int = -19
 
-123456789
+let tutorialTeamInferred = 56
+let tutorialTeamExplicit: Int = 56
+
+let priceInferred = 19.99
+let priceExplicit:Double = 19.99
+
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit:String = "Whoopie Cushion"
+
+if (onSaleInferred) {
+    println("\(nameInferred) on sale for \(priceInferred)!")
+} else {
+    println("\(nameInferred) at regular price: \(priceInferred)!")
+}
 
