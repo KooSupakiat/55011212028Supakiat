9bfc6b8b8db85666d2fd59a0f97dd1a69e44f44e
KooSupakiat <supakiatkoo@gmail.com>
2014-08-20 14:56:14 +0700

    เพิ่มเติมcomment

--- testarray.playground/section-1.swift
+++ testarray.playground/section-1.swift
@@ -4,11 +4,14 @@ shoppingListExplicit = ["Eggs","Milk"]
 
 var shoppingList = ["Eggs","Milk"]
 println("The shopping list contains\(shoppingList.count) items.")
+//นับ
 shoppingList.append("Flour")
 shoppingList += ["Baking Power"]
 shoppingList += ["Chocolate Spreed","Cheese","Butter"]
-var firstItem = shoppingList[0]
+//เพิ่มข้อมมูล
+var firstItem = shoppingList[1]
 shoppingList[0] = "Six eggs"
+//เปลี่ยนข้อมูล
 
 
 
