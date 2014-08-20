b73a8311797c19227a431637c79ea81da9f51936
KooSupakiat <supakiatkoo@gmail.com>
2014-08-20 08:42:49 +0700

    จำนวณข้อมูลในarray

--- testarray.playground/section-1.swift
+++ testarray.playground/section-1.swift
@@ -1,1 +1,8 @@
 var shoppingListExplicit = [String]()
+shoppingListExplicit = ["Eggs","Milk"]
+//var shoppingListExplicit: [String] = ["Eggs","Milk"]
+
+var shoppingList = ["Eggs","Milk"]
+println("The shopping list contains\(shoppingList.count) items.")
+
+
