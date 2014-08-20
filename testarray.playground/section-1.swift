cf51a625693715d654ff29f7ef2c3cf97c5554c3
KooSupakiat <supakiatkoo@gmail.com>
2014-08-20 14:52:42 +0700

    complet array

--- testarray.playground/section-1.swift
+++ testarray.playground/section-1.swift
@@ -4,5 +4,11 @@ shoppingListExplicit = ["Eggs","Milk"]
 
 var shoppingList = ["Eggs","Milk"]
 println("The shopping list contains\(shoppingList.count) items.")
+shoppingList.append("Flour")
+shoppingList += ["Baking Power"]
+shoppingList += ["Chocolate Spreed","Cheese","Butter"]
+var firstItem = shoppingList[0]
+shoppingList[0] = "Six eggs"
+
 
 
