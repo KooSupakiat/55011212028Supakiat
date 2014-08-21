95d791a7d31c92c4f37b8ad587059986c9a36e84
KooSupakiat <supakiatkoo@gmail.com>
2014-08-21 23:18:22 +0700

    เปลี่ยนจาก var เป็น let เกิด error

--- testarray.playground/section-1.swift
+++ testarray.playground/section-1.swift
@@ -1,17 +1,21 @@
-var shoppingListExplicit = [String]()
+let shoppingListExplicit = [String]()
 shoppingListExplicit = ["Eggs","Milk"]
 //var shoppingListExplicit: [String] = ["Eggs","Milk"]
 
-var shoppingList = ["Eggs","Milk"]
+let shoppingList = ["Eggs","Milk"]
 println("The shopping list contains\(shoppingList.count) items.")
 //นับ
 shoppingList.append("Flour")
 shoppingList += ["Baking Power"]
 shoppingList += ["Chocolate Spreed","Cheese","Butter"]
 //เพิ่มข้อมมูล
-var firstItem = shoppingList[1]
+let firstItem = shoppingList[1]
 shoppingList[0] = "Six eggs"
 //เปลี่ยนข้อมูล
+
+
+
+
 
 
 
