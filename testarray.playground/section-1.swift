df816ebc492f1b262ee6ce405fe4e904d443170b
KooSupakiat <supakiatkoo@gmail.com>
2014-08-21 23:22:45 +0700

    เปลี่ยนคืนเป็น var

--- testarray.playground/section-1.swift
+++ testarray.playground/section-1.swift
@@ -1,15 +1,15 @@
-let shoppingListExplicit = [String]()
+var shoppingListExplicit = [String]()
 shoppingListExplicit = ["Eggs","Milk"]
 //var shoppingListExplicit: [String] = ["Eggs","Milk"]
 
-let shoppingList = ["Eggs","Milk"]
+var shoppingList = ["Eggs","Milk"]
 println("The shopping list contains\(shoppingList.count) items.")
 //นับ
 shoppingList.append("Flour")
 shoppingList += ["Baking Power"]
 shoppingList += ["Chocolate Spreed","Cheese","Butter"]
 //เพิ่มข้อมมูล
-let firstItem = shoppingList[1]
+var firstItem = shoppingList[1]
 shoppingList[0] = "Six eggs"
 //เปลี่ยนข้อมูล
 
