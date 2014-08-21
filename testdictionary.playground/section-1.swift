ca5ab471713d0f8a5b5784665d767e0912ad7ad3
KooSupakiat <supakiatkoo@gmail.com>
2014-08-22 00:32:05 +0700

    เริ่ม ditionary แล้วเช็คว่ามีข้อมูลหรือไม่

---
+++ testdictionary.playground/section-1.swift
@@ -0,0 +1,17 @@
+let array: Array<Int> = [1,2,3,4]
+let dictionary: Dictionary<String,Int> = ["dog":1, "elephant":2]
+var airports: [String :String] = ["TYO": "TYO","DUB":"Dublin"]
+
+if airports.isEmpty{
+    println("The airport dictionary is empty.")
+}else{
+    println("The airport dictionary is not empty.")
+}
+//เช็คว่าว่างมั้ย
+
+println("The airports dictionary contains\(airports.count) item")
+
+
+
+
+
