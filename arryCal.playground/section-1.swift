7ff129554f7fadfbc62ef472b673ed86738c985a
KooSupakiat <supakiatkoo@gmail.com>
2014-08-22 14:37:33 +0700

    arrival step 1

--- arryCal.playground/section-1.swift
+++ arryCal.playground/section-1.swift
@@ -1,37 +1,15 @@
 class TipCalculator{
-
-    //2
-    let total: Double
-    let taxPct: Double
-    let subtotal: Double
-    
-    //3
-    init(total:Double, taxPct:Double){
-        self.total = total
-        self.taxPct = taxPct
-        subtotal = total / (taxPct + 1)
-    }
-    //4
-    func calcTipWithTipPct(tipPct:Double) ->Double{
-        return subtotal * tipPct
-    }
-    
     //1
-    func returnPossibleTips() -> [Int: Double]{
-        let possibleTipsInferred = [0.15, 0.18, 0.20]
-        let possibleTipExplicit:[Double] = [0.15, 0.18, 0.20]
-        var numberOfIntems = possibleTipsInferred.count
+    func returnPssibleTip() -> [Int: Double]{
+        let possibleTipInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExlicit:[Double] = [0.15, 0.18, 0.20]
+        var number0fItem = possibleTipInferred.count
         
-        //2
-        var retval = [Int: Double]()
-        for possibleTip in possibleTipsInferred{
-            let intPct = Int(possibleTip*100)
-            //3
-            retval[intPct] = calcTipWithTipPct(possibleTip)
-        }
-        return retval
+        var retval = [int: Double]()
+        for possbleTip
+        
     }
+    return retval
+    
 }
-
-//
-let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
+//let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
