4ff23b15cf8f7fdd2cfedb415d78cee5111f8595
KooSupakiat <supakiatkoo@gmail.com>
2014-08-22 14:41:59 +0700

    step 2

--- arryCal.playground/section-1.swift
+++ arryCal.playground/section-1.swift
@@ -1,12 +1,22 @@
 class TipCalculator{
+   
+    //2
+    let total: Double
+    let taxPct: Double
+    let subtotal: Double
+    
     //1
     func returnPssibleTip() -> [Int: Double]{
         let possibleTipInferred = [0.15, 0.18, 0.20]
         let possibleTipsExlicit:[Double] = [0.15, 0.18, 0.20]
         var number0fItem = possibleTipInferred.count
         
-        var retval = [int: Double]()
-        for possbleTip
+        //2
+        var retval = [Int: Double]()
+        for possbleTip in possibleTipInferred{
+            let intPct = Int(possbleTip*100)
+            
+        }
         
     }
     return retval
