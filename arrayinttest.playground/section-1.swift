f1ef7b748c5c93192e49a438e9f59dfd2b20bbc2
KooSupakiat <supakiatkoo@gmail.com>
2014-08-29 13:55:54 +0700

    week 3 อ.เข้า

--- arrayinttest.playground/section-1.swift
+++ arrayinttest.playground/section-1.swift
@@ -1,2 +1,31 @@
 var intarray = [Int]()
-    intarray = [1,2,3,4,5]
+    intarray = [55011212028,55001212099]
+    
+    intarray.append(5)
+    intarray
+    
+    intarray[0] += 2
+    intarray
+    
+    intarray.append(55011212088)
+    intarray += [55011212077]
+    
+    intarray.insert(0, atIndex: 0)
+    intarray[2...4] = [0,0]
+    
+    intarray.removeLast()
+    
+if (true){
+    
+}
+    
+    
+    
+    /////////////ชื่อ/////////////
+    var namearray = [String]()
+    namearray = ["Supakiat","kookee"]
+    
+    namearray += ["Jangpuangsee"]
+    namearray += ["puipui"]
+    
+    
