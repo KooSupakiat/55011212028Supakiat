127622969f32b9f40f54fb6a553ec83d9e71c1bd
KooSupakiat <supakiatkoo@gmail.com>
2014-08-29 15:31:11 +0700

    สวนสัตว์แก้เป็นclass

--- zoo.playground/section-1.swift
+++ zoo.playground/section-1.swift
@@ -1,14 +1,47 @@
-
-
-func foodzoo(animal: String) ->String{
+class foodanimal{
+    let animal: String
+//    let status: Int
+    let change: String
     var food: [String: String] = ["lion":"meat","cow":"grass","tiger":"mest","Elephant":"sugarcane"]
-   
-    var foodani=food[animal]
-return foodani!
-}
 
-println(foodzoo("lion"))
-println(foodzoo("cow"))
+    
+    init(animal:String, change: String){
+        self.animal = animal
+//        self.status = status
+        self.change = change
+    }
+    
+    
+    func findfood(animal: String) -> String{
+        var namefood = food[animal]
+        return namefood!
+    }
+    
+    func changefood(animal: String,change: String){
+        food[animal] = change
+        
+    }
+}
+let inputanimal = foodanimal(animal: "lion", change: "milk")
+inputanimal.findfood("lion")
+inputanimal.changefood("lion", change: "milk")
+
+
+
+
+
+
+
+
+//func foodzoo(animal: String) ->String{
+//    var food: [String: String] = ["lion":"meat","cow":"grass","tiger":"mest","Elephant":"sugarcane"]
+//   
+//    var foodani=food[animal]
+//return foodani!
+//}
+//
+//println(foodzoo("lion"))
+//println(foodzoo("cow"))
 
 
 //var food: [String: String] = ["lion":"meat","cow":"grass"]
