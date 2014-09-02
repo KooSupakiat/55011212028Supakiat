dc31dab31598ed9d63b16542ebec5e7ab4e60f14
KooSupakiat <supakiatkoo@gmail.com>
2014-09-03 00:27:36 +0700

    start if

---
+++ testif.playground/section-1.swift
@@ -0,0 +1,10 @@
+let orangesAreOrange = true
+let turnipsAreDelicious = false
+
+if turnipsAreDelicious{
+    println("tasty turnips")
+}
+    else{
+        println("turnips are horrible")
+    }
+
