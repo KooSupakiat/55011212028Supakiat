3edd975acf766591a14e3eb1a09329443eae092f
KooSupakiat <supakiatkoo@gmail.com>
2014-09-03 00:41:25 +0700

    testnumberif

--- testif.playground/section-1.swift
+++ testif.playground/section-1.swift
@@ -5,8 +5,16 @@ let turnipsAreDelicious = false
 if turnipsAreDelicious{
     println("tasty turnips")
 }
-    else{
-        println("turnips are horrible")
-    }
+else{
+    println("turnips are horrible")
+}
 
 // prints "Eww, turnips are horrible."
+
+let i=2
+if i==2{
+    println("true")
+}
+else{
+    println("fales")
+}
