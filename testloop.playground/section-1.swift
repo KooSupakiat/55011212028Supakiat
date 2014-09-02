a66e3a196b1b814d08b0539482fe22c2162a94f3
KooSupakiat <supakiatkoo@gmail.com>
2014-09-03 01:43:57 +0700

    สูตรคูณ

--- testloop.playground/section-1.swift
+++ testloop.playground/section-1.swift
@@ -1,9 +1,21 @@
 //loop
 
 var i = 1
+println("start\(i)")
+
 while i < 5{
+    println("round\(i)")
     i = i+1
-    println("--\(i)")
 }
-i
+
+
+let m=2
+var im=1
+
+while im <= 12{
+    var av = m*im
+    println("\(m) * \(im) = \(av)")
+    im = im+1
+}
+
 
