930d0cb819cb47ace599a2b0240350bfaff8afe9
55011212028Supakiat <imacatMSU>
2014-09-03 15:39:58 +0700

    processgrade

--- labclassmix.playground/section-1.swift
+++ labclassmix.playground/section-1.swift
@@ -5,7 +5,7 @@ class Inputrusume{
     let point: Int
     var dicUser: [String: String]
     var dicpoint: Dictionary<String,Int>
-    
+    var dicgrade: Dictionary<String,String>
     
     init(name: String, id:String, point:Int){
         self.name = name
@@ -13,6 +13,7 @@ class Inputrusume{
         self.point = point
         dicUser=[id:name]
         dicpoint=[id:point]
+        self.dicgrade=[String:String]()
     }
     
     func createUser(name:String, id:String){
@@ -26,25 +27,47 @@ class Inputrusume{
         dicpoint[id] = point
     }
     func processgrade(id:String){
-        var
+        var grade:String
+        
+
         let propoint = dicpoint[id]
-        switch propoint{
-            case <50
-            
+        println("//////pro \(propoint)")
+        
+        if propoint<50{
+            grade = "F"
+        }
+        if propoint<60{
+            grade = "D"
+        }
+        if propoint<70{
+            grade = "C"
+        }
+        if propoint<80{
+            grade = "B"
+        }
+        else{
+            grade = "A"
         }
         
+        dicgrade[id] = grade
     }
-    
-    
-    
-    
-    
-    
+    func show
+   
 }
+    
+    
+
+    
+
 let inputdata = Inputrusume(name: "kookee", id: "001", point:80)
 inputdata.createUser("koo", id: "002")
 inputdata.countUser()
 inputdata.pointUser("002", point:75)
+inputdata.processgrade("001")
+
+
+
+
 /////
 
 
