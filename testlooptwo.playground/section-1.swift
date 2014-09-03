8e91ff6926b3567c4af8bd3a3fe09cad14353d9f
KooSupakiat <supakiatkoo@gmail.com>
2014-09-03 10:33:22 +0700

    เพิ่มการแสดงผลในแต่ล่ะการทำงาน

--- testlooptwo.playground/section-1.swift
+++ testlooptwo.playground/section-1.swift
@@ -1,5 +1,5 @@
 func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
-    var min = scores[0]
+    var min = scores[0]//กำหนดค่าแรก
     println(min)
     var max = scores[0]
     println(max)
@@ -7,10 +7,15 @@ func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
     var sum = 0
     
     for score in scores {
+        println("ipscore \(score)")
         if score > max {
+            println("score \(score)")
             max = score//หาค่ามากสุด
+            println("max \(max)")
         } else if score < min {
+             println("score \(score)")
             min = score//หาค่าน้อยสุด
+            println("max \(min)")
         }
         sum += score//หาผลรวม
     }
