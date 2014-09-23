da4f3a7aacb3f69bb4ab59a8991a5c03638ada40
KooSupakiat <supakiatkoo@gmail.com>
2014-09-24 00:46:32 +0700

    protocal

--- Tuple.playground/section-1.swift
+++ Tuple.playground/section-1.swift
@@ -1,7 +1,7 @@
     let  tipAndTotalNamed = (tipAmt:4.00, total:25.19)
     tipAndTotalNamed.tipAmt
     tipAndTotalNamed.total
-
+    
 
 ////////random//////////
 
@@ -14,10 +14,28 @@
 /////////toInt//////////
     let string = "42"
     if let number = string.toInt(){
-        println("Got the number : " \(number))
+        println("Got the number : \(number)")
     } else {
         println("Couldn't convert to a number")
     }
     
-    //
+    // prints "Got the number : 42"
+    
+//////////protocal////////
+    
+    protocol Speaker {
+        func Speak()
+    }
+    
+    class Vicki: Speaker {
+        func Speak() {
+            println("Hello, I am Vicki!")
+        }
+    }
+    
+    class Ray: Speaker {
+        func Speak() {
+            println("Yo, I am Ray")
+        }
+    }
     
