2795c9ed353ec5b01a748d8282230a8b6d96128e
55011212028Supakiat <imacatMSU>
2014-09-24 15:17:41 +0700

    finish lab4 tableView

--- IOSBSwiftPrototypeCellsTutorial/IOSBSwiftPrototypeCellsTutorial/ViewController.swift
+++ IOSBSwiftPrototypeCellsTutorial/IOSBSwiftPrototypeCellsTutorial/ViewController.swift
@@ -8,23 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate ,UITableViewDataSource {
-    let cellIdentifer = "cellIdentifer"
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableView: UITableView!
+
+    
+    
+    let cellIdentifier = "cellIdentifier"
     var tableData = ["bus", "Helicopter", "Truck"]
     
-    @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.tableView.registerClass(UICollectionViewCell.self, forCellReuseIdentifier: self.cellIdentifer)
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-}
+
 
 
 
@@ -34,37 +37,33 @@ func numberOfSectionInTableView(tableView: UITableView!) -> Int{
     return 1
 }
 
-func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int{
+func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
     return tableData.count
 }
 
-func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     
-    var cell = tableView!.dequeueReusableCellWithIdentifier(self.cellIdentifier) as UITableViewCell
+    var cell = tableView.dequeueReusableCellWithIdentifier(self.cellIdentifier) as UITableViewCell
     
-    cell.textLabel!.text = self.tableData[indexPath.row]
+    cell.textLabel!.text = tableData[indexPath.row]
     var imageName = UIImage(named: tableData[indexPath.row])
     cell.imageView!.image = imageName
     return cell
 }
-func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!){
-    
-}
+
 
 //UITableViewDalegate methods
 
-func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
     
     let alert = UIAlertController(title: "Item selected", message: "You selected item \(indexPath.row)", preferredStyle: UIAlertControllerStyle.Alert)
     
-    alert.addAction(UIAlertAction(title: "OK",
-                                  style: UIAlertActionStyle.Default,
-                       handler:{ (alert: UIAlertAction!) in println("An alert of type \(alert.style.hashValue) was tapped")
+    alert.addAction(UIAlertAction(title: "OK",style: UIAlertActionStyle.Default, handler:{ (alert: UIAlertAction!) in println("An alert of type \(alert.style.hashValue) was tapped")
         }))
-    self.presentViewController(alert, true, completion: nil)
+    self.presentViewController(alert, animated:  true, completion: nil)
 }
 
-
+}
 
 
 
