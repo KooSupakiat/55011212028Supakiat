8d2370a814679a7e8ef7e1f9441ee08355c956bd
54011212057Chatchai <imac>
2014-10-17 14:27:58 +0700

    mysegue

---
+++ twoview/twoview/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  twoview
+//
+//  Created by student on 10/17/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var colorlabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
