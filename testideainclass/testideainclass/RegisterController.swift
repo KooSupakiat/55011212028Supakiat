982024c37fcce036be55a22d5c46f85744194adb
54011212057Chatchai <imac>
2014-10-24 16:04:13 +0700

    สร้าง koobook

---
+++ testideainclass/testideainclass/RegisterController.swift
@@ -0,0 +1,31 @@
+//
+//  RegisterController.swift
+//  testideainclass
+//
+//  Created by student on 10/24/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class RegisterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+        
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ testideainclass/testideainclass/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  testideainclass
+//
+//  Created by student on 10/24/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var headlabel: UILabel!
+    @IBOutlet var username: UITextField!
+    @IBOutlet var userpassword: UITextField!
+    
+        
+    @IBAction func loginbutton(sender: UIButton) {
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+       
+        
+    }
+
+    
+
+
+}
+
