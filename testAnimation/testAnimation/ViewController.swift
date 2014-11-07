9f7acef45ace5586a04cc4d6de056cd93a535ca3
54011212057Chatchai <imac>
2014-11-07 14:49:08 +0700

    finlab7

--- testAnimation/testAnimation/ViewController.swift
+++ testAnimation/testAnimation/ViewController.swift
@@ -8,18 +8,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollisionBehaviorDelegate{
     
     var square: UIView!
+    var snap: UISnapBehavior!
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    var collision: UICollisionBehavior!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        square = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        square = UIView(frame: CGRect(x: 200, y: 20, width: 50, height: 50))
         square.backgroundColor = UIColor.grayColor()
         view.addSubview(square)
         
+        animator = UIDynamicAnimator(referenceView: view)
+        gravity = UIGravityBehavior(items: [square])
+        animator.addBehavior(gravity)
+        
+        let barrirer = UIView(frame: CGRect(x: 80, y: 300, width: 130, height: 20))
+        collision = UICollisionBehavior(items: [square])
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
+        
+        barrirer.backgroundColor = UIColor.blueColor()
+        view.addSubview(barrirer)
+        
+        
+        collision.addBoundaryWithIdentifier("barrier", forPath: UIBezierPath(rect: barrirer.frame))
+        collision.action = {
+            println("\(NSStringFromCGAffineTransform(self.square.transform))\(NSStringFromCGPoint(self.square.center))")
+        }
+        collision.collisionDelegate = self
+        
+        let itemBehaviour = UIDynamicItemBehavior(items: [square])
+        itemBehaviour.elasticity = 0.7
+        animator.addBehavior(itemBehaviour)
     }
+    
+    
+    func collisionBehavior(behavior: UICollisionBehavior!, beganContactForItem item: UIDynamicItem!, withBoundaryIdentifier identifier: NSCopying!, atPoint p: CGPoint) {
+        println("Boundary contact occurred - \(identifier)")
+        
+        let collidingView = item as UIView
+        collidingView.backgroundColor = UIColor.yellowColor()
+        UIView.animateWithDuration(0.9){
+            collidingView.backgroundColor = UIColor.grayColor()
+            
+        var firstContact = false
+            if (!firstContact){
+                firstContact = true
+                
+                let square = UIView(frame: CGRect(x: 30, y: 0, width: 10, height: 10))
+                square.backgroundColor = UIColor.grayColor()
+                self.view.addSubview(square)
+                
+                self.collision.addItem(square)
+                self.gravity.addItem(square)
+                
+                let attach = UIAttachmentBehavior(item: collidingView, attachedToItem: square)
+                self.animator.addBehavior(attach)
+                
+            }
+        
+        }
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        if (snap != nil){
+            animator.removeBehavior(snap)
+        }
+        
+        let touch = touches.anyObject() as UITouch
+        snap = UISnapBehavior(item: square, snapToPoint: touch.locationInView(view))
+        animator.addBehavior(snap)
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
